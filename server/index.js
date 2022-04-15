a0a6b6ae00e6210e4d7e1e6c65463be954a8e0a8
cmccr <cmccrory548@gmail.com>
2022-04-14 20:42:04 -0600

    got server running correctly

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static("public"))
 app.use(express.static("files"))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../public/landing.html'))
 })
 
 app.get('/api/home', getHome)
