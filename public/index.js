7fe75da6bd3014f2a861b2dc9621e1b76fd44d50
cmccr <cmccrory548@gmail.com>
2022-04-16 21:56:05 -0600

    added form in list.html

---
+++ public/index.js
@@ -0,0 +1,14 @@
+const contactYourAgentBtn = document.querySelector("#contact")
+const buyerBtn = document.querySelector("#buy")
+const listBtn = document.querySelector("#sell")
+
+const baseURL = 'http://localhost:4005/api/agent'
+
+
+
+
+
+
+contactYourAgentBtn.addEventListener('click', )
+buyerBtn.addEventListener('click', )
+listBtn.addEventListener('click', )
