d31947eefef3dd03d83d2260cf38c8ad6535f06f
cmccr <cmccrory548@gmail.com>
2022-04-18 09:39:13 -0600

    added some functions

--- public/index.js
+++ public/index.js
@@ -1,14 +1,33 @@
+const { default: axios } = require("axios")
+
 const contactYourAgentBtn = document.querySelector("#contact")
 const buyerBtn = document.querySelector("#buy")
 const listBtn = document.querySelector("#sell")
 
 const baseURL = 'http://localhost:4005/api/agent'
 
+function getAgentInfo () {
+    axios.get(`${baseURL}/`)
+    .then(res => {
+        
+    })
+}
+
+function buyHome () {
+    axios.get(`${baseURL}/`)
+    .then(res => {
+
+    })
+}
+
+function listHome () {
+    axios.post(`${baseURL}/`)
+    .then(res => {
+
+    })
+}
 
 
-
-
-
-contactYourAgentBtn.addEventListener('click', )
-buyerBtn.addEventListener('click', )
-listBtn.addEventListener('click', )
+contactYourAgentBtn.addEventListener('click', getAgentInfo)
+// buyerBtn.addEventListener('click', buyHome)
+// listBtn.addEventListener('click', listHome)
