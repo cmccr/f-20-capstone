eb548ab9f3efa391a1b8feb44c5d03e9f646185b
cmccr <cmccrory548@gmail.com>
2022-04-18 18:22:03 -0600

    added more functionality to other buttons

--- server/controllers/agentController.js
+++ server/controllers/agentController.js
@@ -5,8 +5,8 @@ const agents = [
         email: "hannahmccrory@kw.com",
         phoneNumber: 8016088518,
         hours: "9:00am-5:30pm MTN",
-        img: "https://images.unsplash.com/photo-1536599018102-9f803c140fc1?auto=format&fit=crop&w=440&h=220&q=60",
-        bio: ""
+        img: "https://media-exp1.licdn.com/dms/image/C4E03AQEVH_c7mCvIWA/profile-displayphoto-shrink_200_200/0/1608063062745?e=1652918400&v=beta&t=Rne43SZy4onxYhb6stxtEVAoY7lt85293fTqz58qfas",
+        bio: "My background starts with Property & Casualty Insurance. I wanted to help people protect their biggest asset, yet that wasn't enough. I wanted to make sure my clients were protected from the beginning when looking for their dream home, to owning it and keeping ownership. My clients are so satisfied with my work that they love to let everyone know why you should work with me1 They are happy to take your phone call and answer your questions about how I will be the best real estate agent that you will forever work with. I can't wait to be your realtor for life!"
     },
 
     {
@@ -15,8 +15,8 @@ const agents = [
         email: "shanepolevoi@kw.com",
         phoneNumber: 8016319954,
         hours: "9:00am-5:30pm MTN",
-        img: "https://images.unsplash.com/photo-1599229526921-4f29d42b0b41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGllcnxlbnwwfHwwfHw%3D&w=1000&q=80",
-        bio: ""
+        img: "https://media-exp1.licdn.com/dms/image/C4D03AQElN8O8l4-s_w/profile-displayphoto-shrink_200_200/0/1553688822532?e=1652313600&v=beta&t=VXRdiKRPiM6QgWvBHVuMwwsMoA3xpuMCX0kFn6taQvk",
+        bio: "Clients come first. I pledge to be in constant comminication with you, keeping you fully informed throughout the entire selling/buying process. I haven't done my job if I haven't left you with an amazing experience. I'm a top producing salesman on our team with over 18 years of experience. Making sure you have a smooth and clean process getting you into your next chapter. Specializing in selling not listing, sold 100% of homes listed in 2015, 2016, 2017, 2018, and 2019. Can't wait to hear from you!"
     },
 
 ]
--- server/controllers/homesController.js
+++ server/controllers/homesController.js
@@ -1,6 +1,7 @@
 const homes = [
     {
         id: 1,
+        img: "https://mail.google.com/mail/u/0?ui=2&ik=47aeb768ad&attid=0.3&permmsgid=msg-f:1730489360480050533&th=1803eee7a3019d65&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ-xPEwS4znFEa-I7sclmmU_yJ59_80w3dzKXZU6QnHESvogbc90lnS6XqykRBZsfkepc8G3OxsN3b-kgYq-8CYIXL5nDdMPxbbOUcVI9-6ylLAEotfVkYJIxow&disp=emb&realattid=ii_l25bs5sr2",
         address: "4608 S 2675 W Roy, UT 84067",
         price: "$380,000",
         squareFeet: 1808,
@@ -11,6 +12,7 @@ const homes = [
 
     {
         id: 2,
+        img: "https://mail.google.com/mail/u/0?ui=2&ik=47aeb768ad&attid=0.2&permmsgid=msg-f:1730489360480050533&th=1803eee7a3019d65&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ83u62owv58LKuYBgQaE9IkmoNHXcIiT8STJMJ8bcC_DCySACVR74sd1x3DFmn3v5g9tSVI3Zx-yFs_dEF7gVpHpLHomtfSmF-5Oa_MVDS-Yr6H18AuGyUyNiY&disp=emb&realattid=ii_l25brbch1",
         address: "1047 Boston Ct. North Salt Lake City, UT 84054",
         price: "$675,000",
         squareFeet: 2738,
@@ -21,6 +23,7 @@ const homes = [
 
     {
         id: 3,
+        img: "https://mail.google.com/mail/u/0?ui=2&ik=47aeb768ad&attid=0.1&permmsgid=msg-f:1730489360480050533&th=1803eee7a3019d65&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ_84f8LHkypUbRlc3skQtUzM3QG7gc22aUyisdyJ90BQA9bw0XaXJHn9MkC99EsloF77OvE-NumBPMEA6kpu3yBgCYeTSLJXBau1RgywzXuVHxkojd-FB2l-fE&disp=emb&realattid=ii_l25bqo8d0",
         address: "1408 Binford St. Ogden, UT 84401",
         price: "$595,000",
         squareFeet: 2376,
@@ -31,6 +34,7 @@ const homes = [
 
     {
         id: 4,
+        img: "https://mail.google.com/mail/u/0?ui=2&ik=47aeb768ad&attid=0.5&permmsgid=msg-f:1730489473741295469&th=1803ef0201e70f6d&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ-R4DnV9YW9wifmb8KwpZTk4H2mwg2RSRIXPp5Ft8emYcng2NwJhKwysW_Yhzvpn2O4jwfKvc8ZdpG2kk3XO_n3iUwEzLYVdOfubkJb8j-pLOXndO-ayS4k5DY&disp=emb&realattid=ii_l25bwsxp4",
         address: "5121 S 1950 W Roy, UT 84067",
         price: "$385,000",
         squareFeet: 2104,
@@ -41,6 +45,7 @@ const homes = [
 
     {
         id: 5,
+        img: "https://mail.google.com/mail/u/0?ui=2&ik=47aeb768ad&attid=0.4&permmsgid=msg-f:1730489360480050533&th=1803eee7a3019d65&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ9cARIcTnC43DwnwHebcQ5QU9wRjnSuUW9R9iIcjVYCNYGH9_bIAQhQE8Np7CWKPcYsAzSa47uRSgINxi5S36Nh3tNR6iMcDepLPS7D-4IOpuxMLhrKjMQ-3C4&disp=emb&realattid=ii_l25buia53",
         address: "66 W 200 N American Fork, UT 84003",
         price: "$340,000",
         squareFeet: 1124,
